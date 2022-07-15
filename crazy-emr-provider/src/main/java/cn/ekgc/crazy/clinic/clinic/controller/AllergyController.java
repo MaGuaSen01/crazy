935a13ed4fd16ed6bb36000739df1ab2af89e67e
ASUS <maguasen@163..com>
2022-07-15 12:59:13 +0800

    entity

---
+++ crazy-emr-provider/src/main/java/cn/ekgc/crazy/clinic/clinic/controller/AllergyController.java
@@ -0,0 +1,13 @@
+package cn.ekgc.crazy.clinic.clinic.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <b></b>
+ *
+ * @author ASUS
+ * @data 2022/7/15
+ */
+@RestController("allergyController")
+public class AllergyController  {
+}
